f4cec0f40c8952c02ebd58eeb5edc621e82b537b
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-10-29 12:14:05 +0200

    fourier transform function added

---
+++ Music_Equalizer/functions.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#  ----------------------------------- FOURIER TRANSFORM FUNCTION ---------------------------------------------------
+def fourier_transform(df):
+    # How many time points are needed i,e., Sampling Frequency
+    samplingFrequency   = 100
+
+    # At what intervals time points are sampled
+    samplingInterval       = 1 / samplingFrequency
+
+    # Getting df x_axis and y_axis
+    list_of_columns = df.columns
+    df_x_axis = list(df[list_of_columns[0]])
+    df_y_axis = list(df[list_of_columns[1]])
+
+    # Begin and End time period of the signals
+    beginTime = df[list_of_columns[0]].iat[0] # begin_time
+    endTime = df[list_of_columns[0]].iloc[-1] # end time 
+
+    # Frequency domain representation
+    fourierTransform = np.fft.fft(df_y_axis)/len(df_y_axis)           # Normalize amplitude
+    fourierTransform = fourierTransform[range(int(len(df_y_axis)/2))] # Exclude sampling frequency
+    tpCount     = len(df_y_axis)
+    values      = np.arange(int(tpCount/2))
+    timePeriod  = tpCount/samplingFrequency
+    frequencies = values/timePeriod
+
+    # Create subplot
+    figure, axis = plt.subplots()
+    plt.subplots_adjust(hspace=1)
+
+    # Frequency domain representation
+    axis.set_title('Fourier transform depicting the frequency components')
+    axis.plot(frequencies, abs(fourierTransform))
+    axis.set_xlabel('Frequency')
+    axis.set_ylabel('Amplitude')
+
+    st.plotly_chart(figure,use_container_width=True)
--- Music_Equalizer/main.py
+++ Music_Equalizer/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 import pandas as pd
+import functions as fn
 
 # ------------------------------------------------------------------------------------ User Options
 dataset = st.sidebar.file_uploader(label="Uploading Signal", type = ['csv'])
@@ -8,6 +9,7 @@ dataset = st.sidebar.file_uploader(label="Uploading Signal", type = ['csv'])
 # ------------------------------------------------------------------------------------Calling Main Functions
 if dataset is not None:
     df = pd.read_csv(dataset)
+    fn.fourier_transform(df)
 
 else:
     pass
