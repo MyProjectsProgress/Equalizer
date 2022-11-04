4f0df7c884ac9a4e1af45a5c708909887342ebf7
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-11-04 22:09:59 +0200

    musical instruments equalizer are created

--- Music_Equalizer/functions.py
+++ Music_Equalizer/functions.py
@@ -11,6 +11,7 @@ from scipy.fft import irfft
 from scipy.io.wavfile import write
 from scipy.signal import find_peaks
 import wave
+import IPython.display as ipd
 
 #  ----------------------------------- TRIAL FOURIER TRANSFORM FUNCTION ---------------------------------------------------
 def dataframe_fourier_transform(dataframe):
@@ -18,31 +19,37 @@ def dataframe_fourier_transform(dataframe):
     signal_x_axis = (dataframe.iloc[:,0]).to_numpy() # dataframe x axis
     signal_y_axis = (dataframe.iloc[:,1]).to_numpy() # dataframe y axis
 
-    duration    = signal_x_axis[-1] # the last point in the x axis (number of seconds in the data frame)
-    sample_rate = len(signal_y_axis)/duration # returns number points per second
+    x_philo = np.linspace(0,1,1000)
+    signal_y_axis_philo = np.sin(2*np.pi*5000*x_philo)
 
-    fourier_x_axis = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0])) # returns the frequency x axis after fourier transform
-    fourier_y_axis = rfft(signal_y_axis) # returns complex numbers of the y axis in the data frame
+    # duration    = signal_x_axis[-1] # the last point in the x axis (number of seconds in the data frame)
+    # sample_rate = len(signal_y_axis)/duration # returns number points per second
 
-    peaks = find_peaks(fourier_y_axis) # computes peaks of the signal 
-    peaks_indeces = peaks[0]  # list of indeces of frequency with high peaks
+    fourier_x_axis = rfftfreq(len(signal_y_axis_philo), (x_philo[1]-x_philo[0])) # returns the frequency x axis after fourier transform
+    fourier_y_axis = rfft(signal_y_axis_philo) # returns complex numbers of the y axis in the data frame
+    # st.write(fourier_x_axis)
+    # peaks = find_peaks(signal_y_axis_philo) # computes peaks of the signal 
+    # peaks_indeces = peaks[0]  # list of indeces of frequency with high peaks
 
-    points_per_freq = len(fourier_x_axis) / (sample_rate) # NOT UNDERSTANDABLE 
+    # points_per_freq = len(fourier_x_axis) / (sample_rate) # NOT UNDERSTANDABLE 
     
-    fourier_y_axis = dataframe_creating_sliders(peaks_indeces, points_per_freq, fourier_x_axis, fourier_y_axis) # calling creating sliders function
+    # fourier_y_axis = dataframe_creating_sliders(peaks_indeces, points_per_freq, fourier_x_axis, fourier_y_axis) # calling creating sliders function
 
-    dataframe_fourier_inverse_transform(fourier_y_axis,signal_x_axis)
+    # dataframe_fourier_inverse_transform(fourier_y_axis,signal_x_axis)
+
+    # write("filename.wav", 44100, signal_y_axis)
 
     fig, axs = plt.subplots()
     fig.set_size_inches(14,5)
     plt.plot(fourier_x_axis, np.abs(fourier_y_axis)) #plotting signal before modifying
-    plt.plot(fourier_x_axis[peaks_indeces[:]], np.abs(fourier_y_axis)[peaks_indeces[:]], marker="o") # plotting peaks points
+    # plt.plot(fourier_x_axis[peaks_indeces[:]], np.abs(fourier_y_axis)[peaks_indeces[:]], marker="o") # plotting peaks points
     st.plotly_chart(fig,use_container_width=True)
 
 #  ----------------------------------- DATAFRAME INVERSE FOURIER TRANSFORM ---------------------------------------------------
 def dataframe_fourier_inverse_transform(fourier_y_axis,signal_x_axis):
 
     modified_signal = irfft(fourier_y_axis) # returning the inverse transform after modifying it with sliders
+    # write("filename.wav", 44100, modified_signal)
     fig2, axs2 = plt.subplots()
     fig2.set_size_inches(14,5)
     plt.plot(signal_x_axis,modified_signal) # ploting signal after modifying
@@ -55,54 +62,80 @@ def dataframe_creating_sliders(peaks_indeces,points_per_freq,fourier_x_axis,four
     columns = st.columns(10)
     for index, frequency in enumerate(peak_frequencies): 
         with columns[index]:
-            slider_range = svs.vertical_slider(min_value=0.0, max_value=2.0, default_value=1., step=.1, key=index)
-            st.write(type((slider_range)))
-        # these three lines determine the range that will be modified by the slider
-        # needs more investigation
-        minimum_range = int(points_per_freq * (peaks_indeces[index] - 1.5))
-        maximum_range = int(points_per_freq * (peaks_indeces[index] + 1.5))
-        fourier_y_axis[minimum_range - 1 : maximum_range + 2] *= slider_range
+            slider_range = svs.vertical_slider(min_value=0.0, max_value=2.0, default_value=1.0, step=.1, key=index)
+        if slider_range is not None:
+            fourier_y_axis[peaks_indeces[index]  - 2 : peaks_indeces[index]  + 2] *= slider_range
     return fourier_y_axis
 
 #-------------------------------------- Fourier Transform on Audio ----------------------------------------------------
-def audio_fourier_transform(audio_file):
+def musical_instruments_equalizer(audio_file):
+
+    st.audio(audio_file, format='audio/wav')  # displaying the audio
+    obj         = wave.open(audio_file, 'rb') # creating object 
+    sample_rate = obj.getframerate()          # number of samples per second
+    n_samples   = obj.getnframes()            # total number of samples in the whole audio
+    duration    = n_samples / sample_rate     # duration of the audio file
+    signal_wave = obj.readframes(-1)          # amplitude of the sound
+
+    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)   #
+    signal_x_axis = np.linspace(0, duration, len(signal_y_axis)) #
+
+    yf = rfft(signal_y_axis)                                               # returns complex numbers of the y axis in the data frame
+    xf = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0])) # returns the frequency x axis after fourier transform
+    
+    points_per_freq = len(xf) / (xf[-1]) # NOT UNDERSTANDABLE 
+    
+    slider_range_drum    = st.slider(label='Drum Sound'   , min_value=0, max_value=10, value=1, step=1, key="drum slider")
+    slider_range_timpani = st.slider(label='Timpani Sound', min_value=0, max_value=10, value=1, step=1, key="timpani slider")
+    slider_range_piccolo = st.slider(label='Piccolo Sound', min_value=0, max_value=10, value=1, step=1, key="piccolo slider")
+
+    yf[int(points_per_freq*0)   :int(points_per_freq* 1000)] *= slider_range_drum
+    yf[int(points_per_freq*1000):int(points_per_freq* 2600)] *= slider_range_timpani
+    yf[int(points_per_freq*2700):int(points_per_freq*16000)] *= slider_range_piccolo
+
+    modified_signal         = irfft(yf)                 # returns the inverse transform after modifying it with sliders 
+    modified_signal_channel = np.int16(modified_signal) # returns two channels
+
+    write   ("Equalized_Music.wav", sample_rate, modified_signal_channel) # creates the modified song
+    st.audio("example.wav", format='audio/wav')
+
+#-------------------------------------- Fourier Transform on Audio ----------------------------------------------------
+def general_audio_tranform(audio_file):
 
     st.audio(audio_file, format='audio/wav') # displaying the audio
     obj = wave.open(audio_file, 'rb')
-    sample_rate = obj.getframerate()   # number of samples per second
-    n_samples = obj.getnframes()       # total number of samples in the whole audio
-    duration = n_samples / sample_rate # duration of the audio file
-    signal_wave = obj.readframes(-1)   # amplitude of the sound
+    sample_rate = obj.getframerate()      # number of samples per second
+    n_samples   = obj.getnframes()        # total number of samples in the whole audio
+    duration    = n_samples / sample_rate # duration of the audio file
+    signal_wave = obj.readframes(-1)      # amplitude of the sound
 
-    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int32)
+    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
     signal_x_axis = np.linspace(0, duration, len(signal_y_axis))
-
-    # plotting original audio file
-    fig0 = plt.figure(figsize=[10,6])
-    plt.plot(signal_x_axis, signal_y_axis)
-    st.plotly_chart(fig0)
 
     yf = rfft(signal_y_axis) # returns complex numbers of the y axis in the data frame
     xf = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0])) # returns the frequency x axis after fourier transform
 
-    peaks = find_peaks(yf)   # computes peaks of the signal 
-    peaks_indeces = peaks[0] # indeces of frequency with high peaks
-    points_per_freq = len(xf) / (sample_rate) # NOT UNDERSTANDABLE 
+    # fig0 = plt.figure(figsize=[14,5])
+    # plt.plot(xf, abs(yf))
+    # st.plotly_chart(fig0)
     
-    slider_range = st.slider(label='hehe', min_value=0.0, max_value=2.0, value=1.0, step=.1)
+    points_per_freq = len(xf) / (xf[-1]) # NOT UNDERSTANDABLE 
     
-    # these three lines determine the range that will be modified by the slider
-    target_idx   = int(points_per_freq * (peaks_indeces[100])) 
-    target_idx_2 = int(points_per_freq * (peaks_indeces[300]+2000))
-    yf[target_idx - 100 : target_idx_2 + 200] *= slider_range
+    slider_range_drum    = st.slider(label='Drum Sound'   , min_value=0, max_value=10, value=1, step=1, key="drum slider")
+    slider_range_timpani = st.slider(label='Timpani Sound', min_value=0, max_value=10, value=1, step=1, key="timpani slider")
+    slider_range_piccolo = st.slider(label='Piccolo Sound', min_value=0, max_value=10, value=1, step=1, key="piccolo slider")
 
-    fig, axs = plt.subplots()
-    fig.set_size_inches(14,5)
-    plt.plot(xf, np.abs(yf)) #plotting fourier
-    st.plotly_chart(fig)
+    yf[int(points_per_freq*0)   :int(points_per_freq* 1000)] *= slider_range_drum
+    yf[int(points_per_freq*1000):int(points_per_freq* 2600)] *= slider_range_timpani
+    yf[int(points_per_freq*2700):int(points_per_freq*16000)] *= slider_range_piccolo
+
+    # fig, axs = plt.subplots()
+    # fig.set_size_inches(14,5)
+    # plt.plot(xf, np.abs(yf)) #plotting fourier
+    # st.plotly_chart(fig)
 
     modified_signal = irfft(yf) # returning the inverse transform after modifying it with sliders 
-    tryyy = np.int32(modified_signal)
+    tryyy = np.int16(modified_signal)
 
     write("example.wav", sample_rate, tryyy)
     st.audio("example.wav", format='audio/wav')
--- Music_Equalizer/main.py
+++ Music_Equalizer/main.py
@@ -9,15 +9,20 @@ uploaded_file = st.file_uploader(label="Uploading Signal", type = ['csv',".wav"]
 
 # ------------------------------------------------------------------------------------ Calling Main Functions
 if uploaded_file is not None:
+    # Determining whether the file is csv or wav
+    file_name = uploaded_file.type
+    file_extension = file_name[-3:]
 
     # USER OPTIONS
     radio_button = st.radio("",["Default Signal", "Music", "Vowels", "Arrhythima", "Optional"])
 
     if radio_button == "Default Signal":
-        pass
+        if file_extension == "csv":
+            df = pd.read_csv(uploaded_file)
+            fn.dataframe_fourier_transform(df)
 
     elif radio_button == "Music":
-        pass
+        fn.musical_instruments_equalizer(uploaded_file)
 
     elif radio_button == "Vowels":
         pass
@@ -27,16 +32,6 @@ if uploaded_file is not None:
 
     else:
         pass
-
-    file_name = uploaded_file.type
-    file_extension = file_name[-3:]
-
-    if file_extension == "csv":
-        df = pd.read_csv(uploaded_file)
-        fn.dataframe_fourier_transform(df)
-    else:
-        fn.audio_fourier_transform(uploaded_file)
-
 else:
     pass
 
