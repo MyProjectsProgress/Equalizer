08d3f9361be3fdb9b94e18d1490c68da3076e480
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-11-05 19:50:28 +0200

    arrhthmia modified

--- Music_Equalizer/functions.py
+++ Music_Equalizer/functions.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import streamlit_vertical_slider  as svs
+# import pandas as pd
+# import streamlit_vertical_slider  as svs
 from scipy.misc import electrocardiogram
 from scipy.fft import rfft, rfftfreq
 from scipy.fft import irfft
@@ -10,10 +10,10 @@ from scipy.io.wavfile import write
 import wave
 
 #-------------------------------------- UNIFORM RANGE MODE ----------------------------------------------------
-def uniform_range_mode(column1, column2, column3, uploaded_file):
+def uniform_range_mode(column1, column2, column3):
 
-    column1.audio  (uploaded_file, format='audio/wav') # displaying the audio
-    obj = wave.open(uploaded_file, 'rb')
+    column1.audio  (".piano_timpani_piccolo_out.wav", format='audio/wav') # displaying the audio
+    obj = wave.open(".piano_timpani_piccolo_out.wav", 'rb')
     sample_rate   = obj.getframerate()      # number of samples per second
     n_samples     = obj.getnframes()        # total number of samples in the whole audio
     duration      = n_samples / sample_rate # duration of the audio file
@@ -26,19 +26,16 @@ def uniform_range_mode(column1, column2, column3, uploaded_file):
     
     points_per_freq = len(xf) / (xf[-1])                                   # NOT UNDERSTANDABLE 
 
-    plotting_graphs(column2,signal_x_axis, signal_y_axis)
+    # plotting_graphs(column2,signal_x_axis, signal_y_axis, False)
 
     columns=st.columns(10)
     index=0
     list_of_sliders_values = []
     while index < 10:
         with columns[index]:
-            sliders = (svs.vertical_slider(key=index, min_value=0, max_value=10,default_value=1, step=1))
-            st.write("slider",index)
+            sliders = columns[index].slider(label="", key=index, min_value=0, max_value=10,value=1, step=1)
         index +=1
         list_of_sliders_values.append(sliders)
-
-    st.write(list_of_sliders_values)
     
     for indexxx,value in enumerate(list_of_sliders_values):
         if value is not None:
@@ -49,16 +46,16 @@ def uniform_range_mode(column1, column2, column3, uploaded_file):
     modified_signal         = irfft(yf)                 # returns the inverse transform after modifying it with sliders 
     modified_signal_channel = np.int16(modified_signal) # returns two channels
     
-    plotting_graphs(column3,signal_x_axis, modified_signal)
+    # plotting_graphs(column3,signal_x_axis, modified_signal, False)
 
     write(".Equalized_Music.wav", sample_rate, modified_signal_channel) # creates the modified song
     st.audio(".Equalized_Music.wav", format='audio/wav')
 
 #-------------------------------------- MUSICAL INSTRUMENTS EQUALIZER ----------------------------------------------------
-def musical_instruments_equalizer(audio_file):
+def musical_instruments_equalizer():
 
-    st.audio(audio_file, format='audio/wav')  # displaying the audio 
-    obj         = wave.open(audio_file, 'rb') # creating object 
+    st.audio('.piano_timpani_piccolo_out.wav', format='audio/wav')  # displaying the audio 
+    obj         = wave.open(".piano_timpani_piccolo_out.wav", 'rb') # creating object 
     sample_rate = obj.getframerate()          # number of samples per second
     n_samples   = obj.getnframes()            # total number of samples in the whole audio
     duration    = n_samples / sample_rate     # duration of the audio file
@@ -96,40 +93,33 @@ def musical_instruments_equalizer(audio_file):
     st.audio(".Equalized_Music.wav", format='audio/wav')
 
 #-------------------------------------- MEDICAL APPLICATION ----------------------------------------------------
-def arrhythima():
+def arrhythima(column1, column2, column3):
 
-    ecg = electrocardiogram()
+    ecg = electrocardiogram()       # Calling the arrhythmia database of a woman
+    fs = 360                        # determining f sample
+    time = np.arange(ecg.size) / fs # detrmining tima axis
 
-    fs = 360
-    time = np.arange(ecg.size) / fs
+    fourier_x_axis = rfftfreq(len(ecg), (time[1]-time[0])) # Computing fourier x axis
+    fourier_y_axis = rfft(ecg)                             # Computing fourier y axis
 
-    fourier_x_axis = rfftfreq(len(ecg), (time[1]-time[0]))
-    fourier_y_axis = rfft(ecg)
+    points_per_freq = len(fourier_x_axis) / (fourier_x_axis[-1]) # Duration
 
-    points_per_freq = len(fourier_x_axis) / (fourier_x_axis[-1])
+    fourier_y_axis[int(points_per_freq*1) : int(points_per_freq* 5)] *= 0 
 
-    fourier_y_axis[int(points_per_freq*1)   :int(points_per_freq* 5)] *= 0
+    plotting_graphs(column2,time,ecg,True)
 
-    modified_signal         = irfft(fourier_y_axis) 
+    modified_signal = irfft(fourier_y_axis) 
 
-    fig, axs = plt.subplots()
-    fig.set_size_inches(14,5)
-
-    plt.plot(time, (modified_signal))
-    plt.xlabel("Time in s")
-    plt.ylabel("ECG in mV")
-    plt.xlim(45, 51)
-
-    st.plotly_chart(fig)
+    plotting_graphs(column3, time, modified_signal, True)
 
 #-------------------------------------- PLOTING ----------------------------------------------------
-def plotting_graphs(column,x_axis,y_axis):
+def plotting_graphs(column,x_axis,y_axis,flag):
     
     fig, axs = plt.subplots()
     fig.set_size_inches(6,3)
     plt.plot(x_axis,y_axis)
-    # plt.xlabel("time in s")
-    # plt.ylabel("ECG in mV")
+    if flag == True:
+        plt.xlim(46, 50)
+        plt.xlabel("Time in s")
+        plt.ylabel("ECG in mV")
     column.plotly_chart(fig)
-
-
--- Music_Equalizer/main.py
+++ Music_Equalizer/main.py
@@ -1,45 +1,36 @@
 import streamlit as st
-import pandas as pd
 import functions as fn
 
 st.set_page_config(layout="wide")
-with open('Music_Equalizer\main.css') as fileStyle:
+with open('main.css') as fileStyle:
     st.markdown(f'<style>{fileStyle.read()}</style>', unsafe_allow_html=True)
 
 #------------------------------------------------------------------------------------- COLUMNS
-
 column1,column2,column3=st.columns([1,3,3])
 
 # ------------------------------------------------------------------------------------ Uploaded File Browsing Button
 uploaded_file = column1.file_uploader(label="Uploading Signal", type = ['csv',".wav"])
 
 #------------------------------------------------------------------------------------- USER OPTIONS
-
 select_mode = column1.selectbox("",[ "Default","Music", "Vowels", "Arrhythima", "Optional"])
 
 # ------------------------------------------------------------------------------------ Calling Main Functions
 if uploaded_file is not None:
-
-    # Determining whether the file is csv or wav
     file_name = uploaded_file.type
     file_extension = file_name[-3:]
 
-    if select_mode =="Default":
-        fn.uniform_range_mode(column1, column2, column3, uploaded_file)
+if select_mode == "Default":
+    fn.uniform_range_mode(column1, column2, column3)
 
-    elif select_mode == "Music":
-        if file_extension == "wav":
-            fn.musical_instruments_equalizer(uploaded_file)
+elif select_mode == "Music":
+    fn.musical_instruments_equalizer()
 
-    elif select_mode == "Vowels":
-        pass
+elif select_mode == "Vowels":
+    pass
 
-    elif select_mode == "Arrhythima":
-            fn.arrhythima()
-
-    else:
-        pass
+elif select_mode == "Arrhythima":
+    fn.arrhythima(column1, column2, column3)
 
 else:
-    # fn.uniform_range_mode(column1, column2, column3)
-    fn.arrhythima()
+    pass
+
