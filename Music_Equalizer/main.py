062064e314a1a4221064faf688b9d92cb1a1bf22
Mo-Moustafa <momoustafa227@gmail.com>
2022-11-15 01:41:50 +0200

    Edits

--- Music_Equalizer/main.py
+++ Music_Equalizer/main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import functions as fn
-import pandas as pd
 from scipy.fft import irfft
 from scipy.io.wavfile import write
 import numpy as np
@@ -11,32 +10,31 @@ st.set_page_config(layout="wide")
 with open('main.css') as fileStyle:
     st.markdown(f'<style>{fileStyle.read()}</style>', unsafe_allow_html=True)
 
-column1, space, column2=st.columns([1.8,0.2,6])
+tools_col, space, graphs_col=st.columns([1.8,0.2,6])
 
 #-------------------------------------------------------------------- FILE UPLOADER & MODE --------------------------------------------------
-with column1:
+with tools_col:
     st.markdown("""
             <h3 style='text-align: center; margin-bottom: 0px; margin-top:-15px'>
             Equalizer Studio
             </h3>""", unsafe_allow_html=True
             )
 
-uploaded_file = column1.file_uploader(label="", type = ['csv',".wav"])
+uploaded_file = tools_col.file_uploader(label="", type = ['csv',".wav"])
 
-select_mode   = column1.selectbox("Mode", ["Default","Music", "Vowels", "Arrhythima", "Voice Tone Changer"])
+select_mode   = tools_col.selectbox("Mode", ["Default","Music", "Vowels", "Arrhythmia", "Voice Tone Changer"])
 
-show_spectro  = column1.checkbox("Show Spectrogram")
-
-# ------------------------------------------------------------------- GETTING FILE EXTENSION --------------------------------------------------
+# ------------------------------------------------------------------- GETTING FILE NAME --------------------------------------------------
 if uploaded_file is not None:
-    file_type = uploaded_file.type
-    file_extension = file_type[-3:]
+    # file_type = uploaded_file.type
+    # file_extension = file_type[-3:]
     file_name = uploaded_file.name
+    
 
 #-------------------------------------------------------------------- CHOOSING MODE -----------------------------------------------------------
 if select_mode == "Default":
-    uploaded_file = ".piano_timpani_piccolo_out.wav"
-    file_name     = ".piano_timpani_piccolo_out.wav"
+    uploaded_file = "BabyElephantWalk.wav"
+    file_name     = "BabyElephantWalk.wav"
     number_of_sliders = 10
     sliders_labels = ['0 to 1k Hz', '1k to 2k Hz', '2k to 3k Hz','3k to 4k Hz',
     '4k to 5k Hz', '5k to 6k Hz','6k to 7k Hz', '7k to 8k Hz', '8k to 9k Hz','9k to 10k Hz']
@@ -49,19 +47,20 @@ elif select_mode == "Vowels":
     number_of_sliders = 5
     sliders_labels = ['Z','/i:/','/e/','ʊə','F']
 
-
-elif select_mode == "Arrhythima":
-    fn.arrhythima(column2, column1)
+elif select_mode == "Arrhythmia":
+        fn.arrhythmia(graphs_col)
 
 elif select_mode == "Voice Tone Changer":
     if uploaded_file:
-        fn.voice_changer(uploaded_file, column1, column2)
+        fn.voice_changer(uploaded_file, tools_col, graphs_col)
 
 #--------------------------------------------------------------------- MAIN FUNCTION ---------------------------------------------------------
 
-if uploaded_file is not None and select_mode != "Arrhythima" and select_mode != "Voice Tone Changer":
+if uploaded_file is not None and select_mode != "Arrhythmia" and select_mode != "Voice Tone Changer":
 
-    start, pause, resume, space = st.columns([1.001,1.01,0.99,7])
+    show_spectro  = tools_col.checkbox("Show Spectrogram")                                                # Spectrogram Checkbox
+
+    start, pause, resume, space = st.columns([1.001,1.01,0.99,7])                                       # Buttons Columns
 
     signal_x_axis, signal_y_axis, sample_rate = fn.read_audio(uploaded_file)                            # read audio file
 
@@ -69,21 +68,20 @@ if uploaded_file is not None and select_mode != "Arrhythima" and select_mode != 
 
     y_fourier = fn.f_ranges(y_fourier, points_per_freq, number_of_sliders, sliders_labels, select_mode) # create sliders and modify signal
 
-    modified_signal         = irfft(y_fourier)                           # returns the inverse transform after modifying it with sliders
-    modified_signal_channel = np.int16(modified_signal)                  # returns two channels 
+    modified_signal         = irfft(y_fourier)                            # returns the inverse transform after modifying it with sliders
+    modified_signal_to_audio = np.int16(modified_signal)                  # return audio to one channel 
 
-    write(".Equalized_audio.wav", sample_rate, modified_signal_channel)  # creates the modified song
+    write(".Equalized_audio.wav", sample_rate, modified_signal_to_audio)  # creates the modified song
 
-    with column2:
+    with graphs_col:
         if (show_spectro):
-            fn.plot_spectro(file_name,".Equalized_audio.wav")
+            fn.plot_spectro(file_name,".Equalized_audio.wav")                                             # Plot Spectrogram
         else:
             start_btn  = start.button(label='Start')
             pause_btn  = pause.button(label='Pause')
-            resume_btn = resume.button(label='resume')
+            resume_btn = resume.button(label='Resume')
 
-            fn.Dynamic_graph(signal_x_axis,signal_y_axis,modified_signal,start_btn,pause_btn,resume_btn)
+            fn.Dynamic_graph(signal_x_axis,signal_y_axis,modified_signal,start_btn,pause_btn,resume_btn)  # Plot Dynamic Graph
 
-
-    # column2.audio(uploaded_file, format='audio/wav')         # displaying the audio before editing
-    column2.audio(".Equalized_audio.wav", format='audio/wav')  # displaying the audio after  editing
+    # graphs_col.audio(uploaded_file, format='audio/wav')                                                    # displaying the audio before editing
+    graphs_col.audio(".Equalized_audio.wav", format='audio/wav')                                             # displaying the audio after  editing
